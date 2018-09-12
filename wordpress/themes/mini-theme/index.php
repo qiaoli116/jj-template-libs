275de1c02fd25d1628f00f4415eab44f6cb77930
qiao <qiaoli.116@gmail.com>
2018-09-12 11:24:37 +1000

    add method to include part template

--- wordpress/themes/mini-theme/index.php
+++ wordpress/themes/mini-theme/index.php
@@ -7,6 +7,7 @@ d("index.php");
 
 $p = "pass to parts template";
 include( locate_template( 'parts-templates/parts-test.php', false, false ) );
+// or include( get_template_directory().'/parts-templates/parts-test.php' );
 ?>
 
 <?php get_footer(); ?>
