a6f4690202b436809f9351aa3f9e09b04d199de2
qiao <qiaoli.116@gmail.com>
2019-04-03 15:45:26 +1100

    Update page-home.php

--- wordpress/themes/template-site/template-pages/page-home.php
+++ wordpress/themes/template-site/template-pages/page-home.php
@@ -14,11 +14,11 @@ if(have_posts()) {
     while (have_posts() ) { the_post();
         $id = get_the_ID();
         // all other data are accessable with the id
+
         break;
 	}
 	wp_reset_postdata();
 }
-
 ?>
 
 <h1>This is the Home Page - Page id = <?php echo $id;?></h1>
